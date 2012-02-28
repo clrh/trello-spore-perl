806aa834b2170ea47157eb97026dae432f9214c8
Claire <claire.hernandez@gmail.com>
2012-02-28 13:24:59 +0100

    adds cards

--- trello.pl
+++ trello.pl
@@ -7,15 +7,14 @@ use Config::YAML;
 my $conf = Config::YAML->new( config => "conf/mytrello.yaml");
 
 my $client = Net::HTTP::Spore->new_from_spec('spec/trello.json',trace => '1=logs/log_spore_trello.log');
-$client->enable('Format::JSON');
+$client->enable('Net::HTTP::Spore::Middleware::Format::JSON');
 
 # 1st example: https://trello.com/1/boards/<BOARDID>?key=<KEY>&token=<TOKEN>&cards=all
-# gives something like {"id":"4e7279550a78dcdfe520b4f7","name":"sandbox","desc":"","closed":false,"idOrganization":"","pinned":false,"url":"https://trello.com/board/sandbox/4e7279550a78dcdfe520b4f7","prefs":{"invitations":"members","comments":"members","voting":"members","permissionLevel":"public"}}
-
-
 my $cards = $client->boards(
-  id => $conf->get_boardid,
+  id => $conf->get_boardid
 , key => $conf->get_key
+, token => $conf->get_token
+, cards => 'all'
 );
 
 say ">>>End of script ?";

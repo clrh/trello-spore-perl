b47869b4b10e6139c132667f7d38a0ca90cfcf9d
Claire Hernandez <claire.hernandez@biblibre.com>
2012-08-23 11:32:59 +0200

    Spec updated with author and fix hardcoded in trello.pl

--- trello.pl
+++ trello.pl
@@ -13,8 +13,8 @@ $client->enable('Format::JSON');
 
 # 1st example: https://trello.com/1/boards/<BOARDID>?key=<KEY>&token=<TOKEN>&cards=all
 my $cards = $client->get_members_idmember_or_username_boards(
- key => "b79ef04376f5d5e51a8770180fb275e6"
-, token => "4f472f09719da1c5960f2df6208fd168752acea375173b1dfec2e28c891a4b3c"
+ key => $conf->get_key
+, token => $conf->get_token
 , idMember_or_username => "me"
 );
 
